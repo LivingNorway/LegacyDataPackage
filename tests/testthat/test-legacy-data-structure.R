cb269f2b8d51af1be076b9f08ed07567c2b43633
Matthew Grainger <matthew.grainger@nina.no>
2020-05-29 15:23:57 +0200

    Add dependencies

--- tests/testthat/test-legacy-data-structure.R
+++ tests/testthat/test-legacy-data-structure.R
@@ -5,9 +5,9 @@ library(here)
 
 dir_name <- "file-structure"
 
-build_folder_structure(here(dir_name))
+build_folder_structure(dir_name)
 
-test_neet(list.dirs(here(dir_name)), "character")
+test_neet(list.dirs(dir_name), "character")
 
 system("rm -rf file-structure")
 

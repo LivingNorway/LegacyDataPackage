4ac047276334f3a0fb59f4d7ca1e68deb276d955
Joseph Chipperfield <joechip90@googlemail.com>
2020-07-02 01:10:53 +0200

    Removal of Object Library Darwin core object lobrary has now been moved to the R package repository

--- R/ObjectModel/testScript.R
+++
@@ -1,9 +0,0 @@
-objectModelSourceLoc <- paste(Sys.getenv("WORKSPACE_LIVINGNORWAY_DATAPACKAGE"), "R", "ObjectModel", sep = "/")
-source(paste(objectModelSourceLoc, "DCComponent.R", sep = "/"))
-source(paste(objectModelSourceLoc, "DCRecordLevel.R", sep = "/"))
-source(paste(objectModelSourceLoc, "DCOccurrence.R", sep = "/"))
-source(paste(objectModelSourceLoc, "DCOrganism.R", sep = "/"))
-source(paste(objectModelSourceLoc, "DCMaterialSample.R", sep = "/"))
-source(paste(objectModelSourceLoc, "DCEvent.R", sep = "/"))
-source(paste(objectModelSourceLoc, "DCLocation.R", sep = "/"))
-
